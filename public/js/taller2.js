c188c8b4839a66b3b13bc8813be8f9bbcefe5464
SebasGil <8sebasgil@gmail.com>
2019-05-13 22:37:53 -0500

    creacion de arreglo en localStorage

--- public/js/taller2.js
+++ public/js/taller2.js
@@ -1,3 +1,42 @@
+
+
 window.addEventListener("load" , function(){
-feather.replace();
+    feather.replace();
+    
+
+    var listaProductos=[];
+    if(localStorage.getItem("listaProductos")!=null){
+    listaProductos=JSON.parse(localStorage.getItem
+    ("listaProductos"));
+    }
+    //Agregar productos desde la tienda
+    var tienda_agregar = document.querySelectorAll(".producto__mas");
+
+    //Funcion que recorre los botones
+    function tiendaBotones (btn){
+        
+
+
+
+        btn.addEventListener("click" , function(){ 
+            var padre = btn.parentNode.parentNode;
+            var nombre=  padre.querySelector(".nombre").innertext;
+            var precio=  padre.querySelector(".precio").innertext;
+            var categoria=  padre.querySelector(".categoria").value;
+            var imagen=  padre.querySelector(".producto__img").style.backgroundImage;
+            var imagenUrl = imagen.replace('url(','').replace(')','');
+
+            var producto ={
+                nombre:nombre,
+                precio:precio,
+                categoria:categoria,
+                imagen:imagenUrl,
+            };
+
+        });
+    }
+
+    if(tienda_agregar!=null){
+    tienda_agregar.forEach(tiendaBotones);
+    }
 });
