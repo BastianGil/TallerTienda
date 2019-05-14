b0e6b7a01c4dc9f8f0fcbdfaee7dba00f9e1dcb9
SebasGil <8sebasgil@gmail.com>
2019-05-13 23:19:37 -0500

    Botones detalles añaden al localStorage

--- public/js/taller2.js
+++ public/js/taller2.js
@@ -10,8 +10,8 @@ window.addEventListener("load" , function(){
     ("listaProductos"));
     }
     //Agregar productos desde la tienda
-    var tienda_agregar = document.querySelectorAll(".producto__mas");
-    var detalle_agregar = document.querySelector(".producto__mas");
+    var tienda_agregar = document.querySelectorAll(".mas-tienda");
+    var detalles_agregar = document.querySelectorAll(".mas-detalles");
     var num__productos = document.querySelector(".app__NoProducto");
 
 
@@ -44,12 +44,41 @@ window.addEventListener("load" , function(){
 
             listaProductos.push(producto);
             actualizarCarrito();
-            console.log(listaProductos);
+           
 
         });
     }
 
     if(tienda_agregar!=null){
     tienda_agregar.forEach(tiendaBotones);
+    }
+
+    //Botones detalles
+    function detallesBotones (btn){
+        
+
+        btn.addEventListener("click" , function(){ 
+            var nombre=  document.querySelector(".detalles__nombre").innerHTML;
+            var precio=  document.querySelector(".detalles__precio").innerHTML;
+            var categoria=  document.querySelector(".detalles__categoria").value;
+            var imagen=  document.querySelector(".descripcion__imagen").style.backgroundImage;
+            var imagenUrl = imagen.replace('url(','').replace(')','');
+
+            var producto ={
+                nombre:nombre,
+                precio:precio,
+                categoria:categoria,
+                imagen:imagenUrl,
+            };
+
+            listaProductos.push(producto);
+            actualizarCarrito();
+            console.log(listaProductos);
+
+        });
+    }
+
+    if(detalles_agregar!=null){
+    detalles_agregar.forEach(detallesBotones);
     }
 });
